85c4dca808086f57d850bbfde0249fbc65fac25f
MIN <601985329@qq.com>
2019-05-24 22:35:31 +0800

    hrt-120_robot

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/km/catkin_ws/src
+# Build directory: /home/km/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(hrt-120_robot_gazebo)
+subdirs(hrt-120_robot_description)
+subdirs(hrt-120_robot_moveit_config)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/km/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/km/probot_ws/devel;/home/km/ur_ws/devel;/home/km/pr2/devel;/home/km/catkin_ws/devel;/home/km/ws_moveit/devel;/opt/ros/kinetic".split(';') if "/home/km/probot_ws/devel;/home/km/ur_ws/devel;/home/km/pr2/devel;/home/km/catkin_ws/devel;/home/km/ws_moveit/devel;/opt/ros/kinetic" != "" else []
